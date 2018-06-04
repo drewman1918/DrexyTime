ecac1b09916b90966f3565b1817bb3ebfe37d8d5
Drew Bloomfield <DrewBloomfield@Drews-MacBook-Air.local>
2018-06-04 16:15:04 -0600

    Made the 2 week header sticky. Working on the edit memo on the invoice page

--- src/components/TimePerClient/InvoiceRow.js
+++ src/components/TimePerClient/InvoiceRow.js
@@ -1,5 +1,15 @@
 import React, {Component} from 'react';
 import './TimePerClient.css';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { Button } from '@material-ui/core';
 
 export default class InvoiceRow extends Component {
     constructor(){
@@ -16,8 +26,10 @@ export default class InvoiceRow extends Component {
             memoid: '',
             projectid: '',
             employeeid: '',
-            clientid: ''
+            clientid: '',
+            open: false
         }
+        this.handleClickOpen = this.handleClickOpen.bind(this);
     }
 
     componentDidMount(){
@@ -36,13 +48,25 @@ export default class InvoiceRow extends Component {
             clientid: data.clientid
         })
     }
+
+    handleClickOpen = () => {
+        this.setState({ 
+            open: true 
+        });
+      }
+
+    handleClose = () => {
+    this.setState({
+        open: false,
+    });
+    }
     
     render(){
 
         const displayDate = this.state.date.slice(4, 15);
 
         return(
-            <div className = "invoiceDataRow">
+            <div onDoubleClick = { this.handleClickOpen }className = "invoiceDataRow">
                             
                 <div className = "invoiceMemo invoiceDataItem invoiceMemo">{this.state.memo}</div>
 
@@ -55,6 +79,45 @@ export default class InvoiceRow extends Component {
                 <div className = "invoiceRate invoiceDataItem invoiceRate">${this.state.billingrate}</div>
 
                 <div className = "invoiceHours invoiceDataItem invoiceTotal">${(Number(this.state.hours).toFixed(2) * this.state.billingrate).toFixed(2)}</div>
+
+                <div className = "invoicingModal">
+                    <Dialog
+                        open={this.state.open}
+                        onClose={this.handleClose}
+                        aria-labelledby="form-dialog-title"
+                        >
+                        <DialogTitle id="form-dialog-title">Edit Memo</DialogTitle>
+                        <DialogContent>
+
+                            <div className = "editMemoModal">
+                                <textarea value = {this.state.memo}/>
+                            </div>
+
+                            <FormControl fullWidth>
+                            <InputLabel htmlFor="role-simple">Employee</InputLabel>
+                            <Select
+                                inputProps={{
+                                name: 'Employee',
+                                }}
+                                fullWidth
+                            >
+                                <MenuItem value="bill">Hourly</MenuItem>
+                                <MenuItem value="flat">Flat Rate</MenuItem>
+                                <MenuItem value="none">Non-Billing</MenuItem>
+                            </Select>
+                            </FormControl>
+
+                            </DialogContent>
+                        <DialogActions>
+                            <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                            </Button>
+                            <Button onClick={this.addProject} color="primary">
+                            Add
+                            </Button>
+                        </DialogActions>
+                        </Dialog>
+                </div>
 
             </div>
         )
